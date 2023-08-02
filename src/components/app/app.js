e510bb66a5f1a45a7ab14ddd81c323c64046a649
Andrii Balan <balan.andrey.93@gmail.com>
2023-08-02 22:05:01 +0300

    init

---
+++ src/components/app/app.js
@@ -0,0 +1,76 @@
+import { Component } from "react";
+
+import AppInfo from "../app-info/app-info";
+import SearchPanel from "../search-panel/search-panel";
+import AppFilter from "../app-filter/app-filter";
+import EmployeesList from "../employees-list/employees-list";
+import EmployeesAddForm from "../employees-add-form/employees-add-form";
+
+import "./app.css";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [
+                { name: "Andrii Balan", salary: 900, increase: false, id: 1 },
+                {
+                    name: "Anzhelika Balan",
+                    salary: 3400,
+                    increase: true,
+                    id: 2,
+                },
+                {
+                    name: "Mr Kotik Timoshka",
+                    salary: 1100,
+                    increase: false,
+                    id: 3,
+                },
+            ],
+        };
+        this.maxId = 4;
+    }
+
+    deleteItem = (id) => {
+        this.setState(({ data }) => {
+            return {
+                data: data.filter((item) => item.id !== id),
+            };
+        });
+    };
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++,
+        };
+        this.setState(({ data }) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr,
+            };
+        });
+    };
+
+    render() {
+        return (
+            <div className="app">
+                <AppInfo />
+
+                <div className="search-panel">
+                    <SearchPanel />
+                    <AppFilter />
+                </div>
+                <EmployeesList
+                    data={this.state.data}
+                    onDelete={this.deleteItem}
+                />
+                <EmployeesAddForm onAdd={this.addItem} />
+            </div>
+        );
+    }
+}
+
+export default App;
